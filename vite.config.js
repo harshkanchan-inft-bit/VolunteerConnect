29d50acd51d0edbf002672bd971703d8150bde8a
Harsh Kanchan <harshkanchan-inft@atharvacoe.ac.in>
2025-10-05 08:01:35 +0000

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: ["jsmlh2-5173.csb.app"], // add your sandbox host here
+  },
+});
